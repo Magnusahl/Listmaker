6a0cdaf887cafe8c4522571d755f66f04afe00bf
Magnus Ahlqvist <51696488+Magnusahl@users.noreply.github.com>
2020-04-06 15:34:50 +0200

    working save and load

--- app/src/main/java/com/example/listmaker/ListDataManager.kt
+++ app/src/main/java/com/example/listmaker/ListDataManager.kt
@@ -6,7 +6,7 @@ import androidx.preference.PreferenceManager
 class ListDataManager(private val context: Context) {
 
     fun saveList(list: TaskList) {
-        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context).edit()
         sharedPrefs.putStringSet(list.name, list.tasks.toHashSet())
         sharedPrefs.apply()
     }
@@ -14,14 +14,14 @@ class ListDataManager(private val context: Context) {
     fun readLists(): ArrayList<TaskList> {
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
         val contents = sharedPrefs.all
-        val taskList = ArrayList<TaskList>()
+        val taskLists = ArrayList<TaskList>()
 
         for (taskList in contents) {
             val taskItems = ArrayList(taskList.value as HashSet<String>)
             val list = TaskList(taskList.key, taskItems)
-            taskList.add(list)
+            taskLists.add(list)
         }
-        return taskList
+        return taskLists
     }
 
 }
--- app/src/main/java/com/example/listmaker/MainActivity.kt
+++ app/src/main/java/com/example/listmaker/MainActivity.kt
@@ -21,9 +21,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val lists = listDataManager.readLists()
         todoListRecyclerView = findViewById(R.id.lists_recyclerview)
         todoListRecyclerView.layoutManager = LinearLayoutManager(this)
-        todoListRecyclerView.adapter = TodoListAdapter()
+        todoListRecyclerView.adapter = TodoListAdapter(lists)
 
         fab.setOnClickListener { _ ->
             showCreateTodoListDialog()
@@ -61,7 +62,10 @@ class MainActivity : AppCompatActivity() {
         myDialog.setPositiveButton(positiveButtonTitle) {
             dialog, _ ->
                 val adapter = todoListRecyclerView.adapter as TodoListAdapter
-                adapter.addNewItem(todoTitleEditText.text.toString())
+                val list = TaskList(todoTitleEditText.text.toString())
+                listDataManager.saveList(list)
+                adapter.addList(list)
+
                 dialog.dismiss()
         }
         myDialog.create().show()
